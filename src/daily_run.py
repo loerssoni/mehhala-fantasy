384afe0f97b669d962161564c25533ae1937623f
loerssoni <lauri.heikka@gmail.com>
2024-10-19 11:37:42 +0000

    rework ranking logic to benchmark for opponents

--- src/daily_run.py
+++ src/daily_run.py
@@ -1,4 +1,8 @@
 def main():
+    """
+    PREP DATA
+    """
+    
     from get_data import load_history, load_current, combine_history, process_y, load_bios, load_team_data
 
     # load_history()
@@ -72,13 +76,13 @@ def main():
     info = []
     for team in teams.team_id.drop_duplicates():
         info.append(q.get_team_info(team.split('.')[-1]))
-
-
+        
+    """
+    CHECK LINEUPS
+    """
     current_team = [t for t in info if t.name.decode('utf8')=='Kiitos ryhmään pääsystä'][0]
     position_lookup = players.set_index(['playerId'], drop=False).pos.apply(literal_eval).to_dict()
     metrics = [m for metrics in PRED_COLS.values() for m in metrics]
-
-
 
     games_list = []
     for k, v in current_schedule.items():
@@ -95,104 +99,107 @@ def main():
     player_games = player_games.dropna()
 
 
-
     date_now = pd.Timestamp.now(tz='America/Los_Angeles').date()
     cats = ['g','a','sog','fow','hit','block','pim','plusmin','ppp', 'ga','win','so','save']
     m = [m for m in current_team.matchups if pd.to_datetime(m.week_end).date() > date_now + pd.Timedelta('1d')][0]
-    print(m.week)
+
     opponent_id = [t.team_key for t in m.teams if t.team_key != current_team.team_key][0]
     dates = pd.date_range(max(pd.to_datetime(m.week_start).date(), date_now), m.week_end)
-
-
-    ranks = preds.copy()
-    ranks['plusmin'] = ranks['goalsfor'] - ranks['goalsaga']
-    ranks['ga'] = -ranks['ga'] / ranks['icetime']
-    ranks = ranks.drop('icetime', axis=1)
-    ranks = ((ranks - ranks.mean())/(ranks.std())).mean(1).rank(pct=True)
-    ranks.name = 'rank'
-
+    date = dates[0]
     week_teams = teams.loc[(pd.to_datetime(teams.index) >= m.week_start)&(pd.to_datetime(teams.index) <= m.week_end)]
 
     ir = [p.player_key for t in info for p in t.roster.players if p.selected_position.date == date_now.strftime('%Y-%m-%d') and 'IR' in p.selected_position.position]
     ir = []
-    current_lineup = teams[(teams.team_id == current_team.team_key)&(teams.index.get_level_values('date') == (date_now + pd.Timedelta('1d')).strftime('%Y-%m-%d'))&(~teams.player_key.isin(ir))]
+    current_lineup = teams[(teams.team_id == current_team.team_key)&(teams.index.get_level_values('date') == (date_now + pd.Timedelta('1d')).strftime('%Y-%m-%d'))]
     current_lineup = current_lineup.merge(players, how='left', on='player_key').playerId.tolist()
 
+    opp_lineup = teams[(teams.team_id == opponent_id)&(teams.index.get_level_values('date') == (date_now + pd.Timedelta('1d')).strftime('%Y-%m-%d'))]
+    opp_lineup = opp_lineup.merge(players, how='left', on='player_key').playerId.tolist() 
+    
+    """
+    COMPUTE BENCHMARKS
+    """
+    starting_teams = teams.loc[(pd.to_datetime(teams.index) == (date + pd.Timedelta('1d')))]
+    preds_st = ((preds - preds.mean())/(preds.std()))
+    all_current_players = players[players.player_key.isin(starting_teams.player_key)].playerId.tolist()
+    all_current_preds = [p for p in all_current_players if p in preds_st.index]
+    baseline_expected = preds_st.loc[all_current_preds, cats].copy()
+    baseline_expected = baseline_expected.mean()
+
+    opp_expected = preds_st.loc[opp_lineup, cats].copy()
+    opp_expected = opp_expected.mean()
+    
+    
+    from scipy import stats
+    def prob_A_greater_than_B(mu_A, mu_B):
+        # Calculate the mean and standard deviation of the difference distribution
+        mean_diff = mu_A - mu_B
+        prob = 1 - stats.norm.cdf(0, loc=mean_diff, scale=1)
+
+        return prob
+    
+    """
+    MAKE PREDICTIONS
+    """
     selected_team = []
 
-    for date in dates:
-        rankings = []
-        print('\n\n\n', date.date())
-        if len(selected_team) > 0:
-        #     current_lineup = [p for p in selected_team] # this is inactive since we use our existing lineup as starting point for each day of the week
-            selected_team = []
+    date = dates[0]
+    rankings = []
+    print('\n\n\n', date.date())
 
-        starting_teams = teams.loc[(pd.to_datetime(teams.index) == (date + pd.Timedelta('1d')))]
-        all_available_players = players[(~players.player_key.isin(starting_teams.player_key))|(players.playerId.isin(current_lineup))]
-        all_available_players = all_available_players.playerId.tolist()
+    all_available_players = players[(~players.player_key.isin(starting_teams.player_key))|(players.playerId.isin(current_lineup))]
+    all_available_players = all_available_players.playerId.tolist()
 
-        week_games = player_games[(player_games.ts > date)&(player_games.ts <= m.week_end)]
+    week_games = player_games[(player_games.ts > date)&(player_games.ts <= m.week_end)]
 
+    own_current = pd.Series({k:0 for k in cats})
 
+    while len(selected_team) < 50:
+        print(str(len(selected_team)), end='\r')
 
-        while len(selected_team) < 50:
-            print(str(len(selected_team)), end='\r')
+        if len(selected_team) < 14:
+            available = [p for p in current_lineup if p not in selected_team]
 
-            if len(selected_team) < 14:
-                available = [p for p in current_lineup if p not in selected_team]
+        else:
+            available = [p for p in all_available_players if p not in selected_team]
 
+        rest_games = lineup_utils.get_rest_of_season_games((date + pd.Timedelta('1d')), player_games, selected_team, position_lookup)
+        stats_available = rest_games[rest_games.index.isin(preds_st.index)].index
+        lineup_preds = preds_st.loc[stats_available, cats].apply(lambda x: x * rest_games[stats_available] / rest_games.mean())
+        added_vals = lineup_preds.apply(lambda x: prob_A_greater_than_B(x, baseline_expected), 1).apply(pd.Series, index=cats)
+        ranks_season = lineup_preds.sum(1)
+        ranks_season.name = 'rank'
+
+        week_rest_games = lineup_utils.get_rest_of_season_games(date, week_games, selected_team, position_lookup)
+        week_stats_available = week_rest_games[week_rest_games.index.isin(preds.index)].index
+        week_lineup_preds = preds_st.loc[stats_available, cats].apply(lambda x: x * week_rest_games[week_stats_available] / week_rest_games.mean())
+        week_added_vals = week_lineup_preds.apply(lambda x: prob_A_greater_than_B(x, opp_expected), 1).apply(pd.Series, index=cats)
+        week_ranks = week_added_vals.sum(1)
+        week_ranks.name = 'week_rank'
+
+        if len(selected_team) < 14:
+            selected_player = ranks_season.loc[[p for p in available if p in ranks_season]].idxmax()
+            selected_team.append(selected_player)
+            own_current = (own_current * len(selected_team) + added_vals.loc[selected_player] * (14-len(selected_team))) / 14
+            own_current = own_current.fillna(0)
+
+            data_dict = {'playerId':selected_player, 
+                         'rank': round(ranks_season.loc[selected_player], 3), 
+                         'games':rest_games.loc[selected_player]}
+            if selected_player in week_stats_available:
+                data_dict['week_rank'] = round(week_ranks.loc[selected_player], 3)
+                data_dict['week_games'] = week_rest_games.loc[selected_player]
             else:
-                available = [p for p in all_available_players if p not in selected_team]
+                data_dict['week_rank'] = 0
+                data_dict['week_games'] = 0
+            rankings.append(data_dict)
 
-            rest_games = lineup_utils.get_rest_of_season_games((date + pd.Timedelta('1d')), player_games, selected_team, position_lookup)
-            stats_available = rest_games[rest_games.index.isin(preds.index)].index
-            lineup_preds = preds.loc[stats_available, cats].apply(lambda x: x * rest_games[stats_available])
-            preds_st = ((lineup_preds - lineup_preds.mean())/(lineup_preds.std()))
-            ranks = preds_st.mean(1).rank(pct=True)
-            ranks.name = 'rank'
-
-            week_rest_games = lineup_utils.get_rest_of_season_games(date, week_games, selected_team, position_lookup)
-            week_stats_available = week_rest_games[week_rest_games.index.isin(preds.index)].index
-            week_lineup_preds = preds.loc[stats_available, cats].apply(lambda x: x * week_rest_games[week_stats_available])
-            week_preds_st = ((week_lineup_preds - week_lineup_preds.mean())/(week_lineup_preds.std()))
-            week_ranks = week_preds_st.mean(1).rank(pct=True)
-            week_ranks.name = 'week_rank'
-
-            if len(selected_team) < 14:
-                selected_player = ranks.loc[[p for p in available if p in ranks]].idxmax()
-                selected_team.append(selected_player)
-                data_dict = {'playerId':selected_player, 
-                             'rank': round(ranks.loc[selected_player], 3), 
-                             'games':rest_games.loc[selected_player]}
-                if selected_player in week_stats_available:
-                    data_dict['week_rank'] = round(week_ranks.loc[selected_player], 3)
-                    data_dict['week_games'] = week_rest_games.loc[selected_player]
-                else:
-                    data_dict['week_rank'] = 0
-                    data_dict['week_games'] = 0
-                rankings.append(data_dict)
-
-            else:
-                rest_of_them = ranks[[p for p in available if p in stats_available]].sort_values(ascending=False).iloc[:(50-len(selected_team))].index.tolist()
-                for p in rest_of_them:
-                    data_dict = {'playerId':p, 
-                             'rank': round(ranks.loc[p], 3), 
-                             'games':rest_games.loc[p]}
-                    if p in week_stats_available:
-                        data_dict['week_rank'] = round(week_ranks.loc[p], 3)
-                        data_dict['week_games'] = week_rest_games.loc[p]
-                    else:
-                        data_dict['week_rank'] = 0
-                        data_dict['week_games'] = 0
-                    rankings.append(data_dict)
-
-                selected_team += rest_of_them
-
-        for p in current_lineup:
-            if p not in selected_team:
+        else:
+            rest_of_them = ranks_season[[p for p in available if p in stats_available]].sort_values(ascending=False).iloc[:(50-len(selected_team))].index.tolist()
+            for p in rest_of_them:
                 data_dict = {'playerId':p, 
-                             'rank': round(ranks.loc[p], 3), 
-                             'games':rest_games.loc[p]}
+                         'rank': round(ranks_season.loc[p], 3), 
+                         'games':rest_games.loc[p]}
                 if p in week_stats_available:
                     data_dict['week_rank'] = round(week_ranks.loc[p], 3)
                     data_dict['week_games'] = week_rest_games.loc[p]
@@ -200,34 +207,53 @@ def main():
                     data_dict['week_rank'] = 0
                     data_dict['week_games'] = 0
                 rankings.append(data_dict)
-                                
-        rankings = pd.DataFrame(rankings).set_index('playerId')
 
-        n_games = week_rest_games[week_rest_games.index.isin(preds.index)]
-        n_games.name = 'n_games'
-        print(player_info.loc[current_lineup])
-        print('DROPS')
-        for p in current_lineup:
-            if p not in selected_team:
+            selected_team += rest_of_them
 
-                print(player_info.join(n_games).join(rankings).loc[p].to_dict())
-        print('ADDS')
-        for p in selected_team:
-            if p not in current_lineup:
-                print(player_info.join(n_games).join(rankings).loc[p].to_dict())
+    for p in current_lineup:
+        if p not in selected_team:
+            data_dict = {'playerId':p, 
+                         'rank': round(ranks_season.loc[p], 3), 
+                         'games':rest_games.loc[p]}
+            if p in week_stats_available:
+                data_dict['week_rank'] = round(week_ranks.loc[p], 3)
+                data_dict['week_games'] = week_rest_games.loc[p]
+            else:
+                data_dict['week_rank'] = 0
+                data_dict['week_games'] = 0
+            rankings.append(data_dict)
 
-        break
+    rankings = pd.DataFrame(rankings).set_index('playerId')
 
-    output = player_info.join(rankings, how='inner').sort_values(['rank'], ascending=False).join(preds_st.round(3)).join(week_preds_st.round(3), rsuffix='_week')
+    n_games = week_rest_games[week_rest_games.index.isin(preds.index)]
+    n_games.name = 'n_games'
+    print(player_info.loc[current_lineup])
+    print('DROPS')
+    for p in current_lineup:
+        if p not in selected_team:
+
+            print(player_info.join(n_games).join(rankings).loc[p].to_dict())
+    print('ADDS')
+    for p in selected_team:
+        if p not in current_lineup:
+            print(player_info.join(n_games).join(rankings).loc[p].to_dict())
+    
+    """
+    SAVE DATA
+    
+    """
+            
+
+    total_values = preds_st[cats].apply(lambda x: prob_A_greater_than_B(x, baseline_expected), 1).apply(pd.Series, index=cats)
+    output = player_info.join(rankings, how='inner').sort_values(['rank'], ascending=False).join(total_values.round(3))
     output['current_lineup'] = output.index.isin(current_lineup)
     output['selection'] = output.index.isin(selected_team)
     output = output.sort_values(['current_lineup', 'rank','week_rank'], ascending=False)
     output.to_csv('data/player_data.csv')
     output.to_csv(f'data/archive/{date_now}_player_data.csv')
 
-    ranks = preds.copy()
-    ranks = ranks.drop('icetime', axis=1)
-    # ranks = ((ranks - ranks.mean())/(ranks.std())).mean(1).rank(pct=True)
+    ranks = preds_st.copy()
+    ranks = ranks[cats]
     full_n_games = player_games[player_games.ts.dt.date > date_now].groupby('playerId').gameId.count()
     full_n_games = pd.DataFrame(ranks).join(full_n_games).fillna(0).gameId
     week_n_games = player_games[(player_games.ts.dt.date > date_now)&(player_games.ts <= m.week_end)].groupby('playerId').gameId.count()
@@ -254,6 +280,9 @@ def main():
     ss = ss.sort_values(['own','matchup'], ascending=False)
     ss.to_csv('data/team_data.csv')
     ss.to_csv(f'data/archive/{date_now}_team_data.csv')
+
+        
+    
 
 if __name__ == '__main__':
     main()
