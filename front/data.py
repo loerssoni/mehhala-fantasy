5dedd7db8c056dc496b56838b2fc6c14f52bcf2e
loerssoni <53056217+loerssoni@users.noreply.github.com>
2024-10-25 21:56:05 +0300

    Update data.py

--- front/data.py
+++ front/data.py
@@ -6,9 +6,9 @@ player_data = pd.read_csv('gs://{}.appspot.com/player_data.csv'.format(project_i
 team_data = pd.read_csv('gs://{}.appspot.com/team_data.csv'.format(project_id))
 
 skater_data = player_data.loc[player_data.pos != "['G']", ['is_available', 'current_lineup','name','pos','rank','week_rank',
-                              'week_games','g','a','sog','ppp', 'fow','hit','block','pim','plusmin','team']]
+                              'week_games','g','a','sog','ppp', 'fow','hit','block','pim','plusmin','team', 'games']]
 goalie_data = player_data.loc[player_data.pos == "['G']",['is_available', 'current_lineup','name','rank','week_rank','week_games', 
-                                          'ga','win','so','save','team']]
+                                          'ga','win','so','save','team', 'games']]
 team_season_data = team_data[['team','manager','g', 'a', 'ppp', 'sog', 'fow', 'hit', 'block', 'pim', 'plusmin',
        'ga', 'win', 'so', 'save']]
 team_week_data = team_data[['team','manager','g_week', 'a_week', 'ppp_week', 'sog_week', 'fow_week',
